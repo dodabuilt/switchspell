fcc825b1a130a15998665481361793d3ca89dbc2
Dave Medvedev <davemedvedev@gmail.com>
2023-01-30 14:20:54 -0500

    light dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <v-app>
+  <v-app :theme="theme">
+    <v-app-bar>
+      <v-spacer></v-spacer>
+
+      <v-btn :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" @click="onClick">Toggle
+        Theme</v-btn>
+    </v-app-bar>
+
     <v-main>
       <router-view />
     </v-main>
@@ -7,11 +14,18 @@
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: "App",
 
   data: () => ({
-    //
+    theme: ref('light')
   }),
+  methods: {
+    onClick() {
+      this.theme = this.theme === 'light' ? 'dark' : 'light'
+    }
+  }
 };
 </script>
